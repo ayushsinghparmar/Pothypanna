7f6e7571fcb6048179ee05d7a6fba69e628addcb
ayushsinghparmar <103324099+ayushsinghparmar@users.noreply.github.com>
2026-06-29 15:31:52 +0530

    Update Supabase ANON key in admin.js

--- admin/admin/admin/admin.js
+++ admin/admin/admin/admin.js
@@ -1,6 +1,6 @@
 const SUPABASE_URL = "https://vwueomdiatgjkbqxdwzk.supabase.co";
 
-const SUPABASE_ANON_KEY = "YOUR_ANON_KEY";
+const SUPABASE_ANON_KEY = "sb_publishable_9DDEETnQ2I4AtLmal4TNSw_0QmYGnV6";
 
 const supabase = window.supabase.createClient(
     SUPABASE_URL,
